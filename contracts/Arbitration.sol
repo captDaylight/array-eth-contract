994d431e1dfc9171bf450142bce49d0dab1608bf
paul <paul.christophe6@gmail.com>
2017-09-26 15:34:40 -0400

    have the claimant struct in place

--- contracts/Arbitration.sol
+++ contracts/Arbitration.sol
@@ -1,5 +1,19 @@
 pragma solidity ^0.4.4;
 
 contract Arbitration {
+  string description;
 
+  struct Claimant {
+    string opinion;
+    address addr;
+    address arbiter;
+  }
+
+  Claimant[2] claimants;
+  /*mapping(uint => Claimant) public claimants;*/
+
+  function addClaimant(string opinion, address arbiter) {
+    // TODO check that the amount of claimants isn't above 2
+    claimants.push(Claimant(opinion, msg.sender, arbiter));
+  }
 }
