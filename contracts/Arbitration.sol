60052562c4ab0318ae6e71a50d49049a3d9191df
paul <paul.christophe6@gmail.com>
2017-09-27 17:28:30 -0400

    a little bit of progress on the arbitration pag

--- contracts/Arbitration.sol
+++ contracts/Arbitration.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.4;
 
 contract Arbitration {
   string description;
+  address public winner;
 
   struct Claimant {
     string opinion;
@@ -9,11 +10,42 @@ contract Arbitration {
     address arbiter;
   }
 
-  Claimant[2] claimants;
+  Claimant[] public claimants;
   /*mapping(uint => Claimant) public claimants;*/
+
+  function addDescription(string _description) {
+    description = _description;
+  }
 
   function addClaimant(string opinion, address arbiter) {
     // TODO check that the amount of claimants isn't above 2
+
+    // EXPLAIN - what is msg??
     claimants.push(Claimant(opinion, msg.sender, arbiter));
+  }
+
+  function verifyArbiter() returns (bool isArbiter) {
+    for (uint i = 0; i < claimants.length; i++) {
+      if (msg.sender != claimants[i].arbiter) {
+        // error, claimants must agree on the arbiter
+        return false;
+      }
+    }
+    // all claimants have listed the same arbiter
+    return true;
+  }
+
+  function arbiterGetProposals()
+    returns (string description, string proposal_one, string proposal_two)
+  {
+    // require allows us to check for specific conditions, otherwise it throws an error
+    require(claimants.length < 2 && verifyArbiter());
+    return (description, claimants[0].opinion, claimants[1].opinion);
+  }
+
+  function arbiterSelectWinner() {
+    require(verifyArbiter());
+
+
   }
 }
---
+++ contracts/SayHello.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.4;
+
+contract Addition {
+	function addToTen(uint amount) returns (uint sum)
+	{
+		return amount + 8;
+	}
+}
